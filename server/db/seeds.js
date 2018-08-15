25e3ad817d81c09d601563181dce514d9cbb5d57
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-15 10:04:11 +0100

    seed data seeded

---
+++ server/db/seeds.js
@@ -0,0 +1,13 @@
+use bucketlist;
+db.dropDatabase();
+
+db.items.insertMany([
+  {
+    title: "Get a job",
+    complete: "false"
+  },
+  {
+    title: "Go to Japan",
+    complete: "false"
+  }
+]);
